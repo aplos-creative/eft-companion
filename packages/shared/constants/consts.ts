d99a3dbda847d978184cf92ebd12e1153e4b5c33
xavidram <xavidram@gmail.com>
2022-07-06 17:49:57 -0500

    setting up windows as vite proejcts

---
+++ packages/shared/constants/consts.ts
@@ -0,0 +1,22 @@
+const eftClassId = 21634;
+
+const interestingFeatures = [
+  'match_info',
+  'game_info'
+];
+
+const windowNames = {
+  inGame: 'in_game',
+  desktop: 'desktop'
+};
+
+const hotkeys = {
+  toggle: 'eftShowHide'
+};
+
+export {
+  eftClassId,
+  interestingFeatures,
+  windowNames,
+  hotkeys
+}
