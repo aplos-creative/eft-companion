fac5b9fa644fc7e2ecf7d253165b42d96d805f7b
xavidram <xavidram@gmail.com>
2022-07-06 20:38:18 -0500

    adding apollo integration to desktop app

--- packages/common/src/overwolf/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './ow-game-listener';
-export * from './ow-games';
-export * from './ow-games-events';
-export * from './ow-hotkeys';
-export * from './ow-listener';
-export * from './ow-window';
-export * from './timer';
--- windows/desktop/src/main.ts
+++ windows/desktop/src/main.ts
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
+import {
+  ApolloClient,
+  ApolloLink,
+  InMemoryCache,
+  from,
+  HttpLink,
+} from "@apollo/client/core";
+import { ApolloClients } from "@vue/apollo-composable";
 import App from './App.vue'
 import PrimeVue from 'primevue/config';
 
@@ -6,7 +14,28 @@ import 'primevue/resources/themes/tailwind-light/theme.css';
 import 'primevue/resources/primevue.min.css';
 import 'primeicons/primeicons.css'
 
+const additiveLink = from([
+  new ApolloLink((operation, forward) => {
+    operation.setContext(({ headers }: any) => ({
+      headers: {
+        ...headers
+      },
+    }));
+    return forward(operation); // Go to the next link in the chain. Similar to `next` in Express.js middleware.
+  }),
+  new HttpLink({ uri: "https://api.tarkov.dev/graphql" }),
+]);
+
+const apolloClient = new ApolloClient({
+  link: additiveLink,
+  cache: new InMemoryCache(),
+});
+
+
 createApp(App)
 .use(PrimeVue)
+.provide(ApolloClients, {
+  default: apolloClient
+})
 .mount('#app')
 
